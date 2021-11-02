176fc5096699d994ebdffd7654afbf14f159ad1e
hsr <hectorsilvarobles@gmail.com>
2021-11-01 21:05:29 -0700

    fuck

---
+++ src/main/java/com/example/linkedIn_tutorial_spring/LinkedInTutorialSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.linkedIn_tutorial_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedInTutorialSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedInTutorialSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/linkedIn_tutorial_spring/LinkedInTutorialSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linkedIn_tutorial_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedInTutorialSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
